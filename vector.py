35572c44f331a1f94ed9fe9d543e7715f6c08997
ashuto0sh <azbycs@gmail.com>
2016-07-18 22:37:52 +0530

    Add operator __add__, __mul__, __rmul__ and __sub__ to vector.py

--- vector.py
+++ vector.py
@@ -12,12 +12,39 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
+    def __isSameLen(self, v):
+        return self.dimension==v.dimension
+
+    def __add__(self, other):
+        try:
+            if not self.__isSameLen(other):
+                raise ValueError
+        except ValueError:
+            raise ValueError('Addition requires vectors of same length')
+
+        r=[x+y for x, y in zip(self.coordinates, other.coordinates)]
+        return Vector(r)
+
+    def __sub__(self, other):
+        try:
+            if not self.__isSameLen(other):
+                raise ValueError
+        except ValueError:
+            raise ValueError('Subtraction requires vectors of same length')
+
+        r=[x-y for x, y in zip(self.coordinates, other.coordinates)]
+        return Vector(r)
+
+    def __mul__(self, scalar):
+        r=[scalar*x for x in self.coordinates]
+        return Vector(r)
+
+    def __rmul__(self, scalar):
+        r=[scalar*x for x in self.coordinates]
+        return Vector(r)
 
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
-
     def __eq__(self, v):
         return self.coordinates == v.coordinates
-
-
